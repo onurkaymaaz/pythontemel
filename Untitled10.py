b66db38372af8babe80e646f56fad821caa861e9
onurkaymaaz <93345959+onurkaymaaz@users.noreply.github.com>
2022-09-24 16:12:32 +0300

    Add files via upload

---
+++ Untitled10.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def reverseList(input):
+
+    for i in input:
+        if isinstance(i, list):
+            reverseList(i)
+            i=i.reverse()
+        else:
+            inputt.reverse()
+
+inputt = [[1, 2], [3, 4], [5, 6, 7]]
+
+reverseList(inputt)
+print("reverse list: ", inputt)
+
